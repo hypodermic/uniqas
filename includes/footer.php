26c15d1dd4c933d3a6a8b0b7aa67318bc24d96d5
Cecilia De La Fuente <cid41fg@gmail.com>
2019-01-14 22:57:24 -0500

    test 2

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
 				<li><a href="https://www.instagram.com/somosuniqas/" target="_blank">instagram</a></li>
 				<li><a href="https://www.facebook.com/uniqas-205135883444299/" target="_blank">facebook</a></li>
 			</ul>
-			made by cecilia
+			made by cecilia!
 	</footer>
 </body>
 </html>
